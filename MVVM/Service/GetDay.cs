f4c6676ad89bd717e85cccbf9763e36d76262e01
AlimuratNur <alimratnurgajn@gmail.com>
2025-12-02 22:12:51 +0500

    implemented json reader and get  day subjects in services

---
+++ MVVM/Service/GetDay.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ClassShedule.MVVM.Service
+{
+    internal static class GetDay
+    {
+        public static List<string> GetSubjects(List<List<string>> allDays)
+        {
+            var dateNow = DateTime.Now.DayOfWeek;
+            List<string> result;
+            switch (dateNow)
+            {
+                case DayOfWeek.Monday:
+                    result = allDays[0];
+                    break;
+                case DayOfWeek.Tuesday:
+                    result = allDays[1];
+                    break;
+                case DayOfWeek.Wednesday:
+                    result = allDays[2];
+                    break;
+                case DayOfWeek.Thursday:
+                    result = allDays[3];
+                    break;
+                case DayOfWeek.Friday:
+                    result = allDays[4];
+                    break;
+                default:
+                    result = new List<string>();
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MVVM/Service/JsonReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClassShedule.MVVM.Service
+{
+    internal class JsonReader<T>
+    {
+        public async Task<List<T>> LoadAsync(string _FilePath)
+        {
+            if (!File.Exists(_FilePath)) return new List<T>();
+            using var file = File.OpenRead(_FilePath);
+            return await JsonSerializer.DeserializeAsync<List<T>>(file)
+                ?? new List<T>();
+        }
+
+    }
+}
