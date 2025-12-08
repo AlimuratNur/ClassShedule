b37cb0a53ea92f0fb61b87790bb5aef9b60c9fec
AlimuratNur <alimratnurgajn@gmail.com>
2025-12-08 18:57:01 +0500

    implementd subject class

---
+++ MVVM/Model/Subject.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace ClassShedule.MVVM.Model;
+
+internal class Subject
+{
+    public string Name { get; }
+    public DateTime Start { get; }
+    public DateTime End { get; }
+
+    public Subject(string name, DateTime start, DateTime end)
+    {
+        Name = name;
+        Start = start;
+        End = end;
+
+    }
+}
